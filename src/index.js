957dd881e1a36a44797100102db4486289f15b56
Eduardo Gutierrez <eduardo.gutierrez.2325@gmail.com>
2020-07-23 18:03:21 -0500

    Using different component creation approaches

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Title from "./components/Title";
+import Subtitle from "./components/Subtitle";
+
+// Functional Component
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Title />
+      <Subtitle />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
