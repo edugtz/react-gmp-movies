f884ba538b012f2dc49b41ab78a39bda738e36d4
Eduardo Gutierrez <eduardo.gutierrez.2325@gmail.com>
2020-07-23 19:16:04 -0500

    Implemented React.createElement approach

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Title from "./components/Title";
-import Subtitle from "./components/Subtitle";
-
-// Functional Component
-const App = () => {
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <Title />
-      <Subtitle />
-    </div>
-  );
-};
+import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
