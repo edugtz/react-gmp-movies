98c9be281cd7185e823a93192078d676369f6c57
Eduardo Gutierrez <eduardo.gutierrez.2325@gmail.com>
2020-09-27 20:51:43 -0500

    Implemented AddMovie form with Formik and validated inputs

--- src/api/api.js
+++ src/api/api.js
@@ -1,23 +1,25 @@
-import api from 'axios';
+import api from 'axios'
 
-const baseUrl = 'http://localhost:4000';
+const baseUrl = 'http://localhost:4000'
 
 const ROUTES = {
     movies: 'movies',
-};
+}
 
 export const getMovies = ({ sortBy }) => {
-    return api.get(`${baseUrl}/${ROUTES.movies}`, { params: { limit: 100, offset: 1, sortBy, sortOrder: 'desc' } });
-};
+    return api.get(`${baseUrl}/${ROUTES.movies}`, {
+        params: { limit: 9999, offset: 1, sortBy, sortOrder: 'desc' },
+    })
+}
 
 export const createMovie = (data) => {
-    return api.post(`${baseUrl}/${ROUTES.movies}`, data);
-};
+    return api.post(`${baseUrl}/${ROUTES.movies}`, data)
+}
 
 export const updateMovie = (data) => {
-    return api.put(`${baseUrl}/${ROUTES.movies}`, data);
-};
+    return api.put(`${baseUrl}/${ROUTES.movies}`, data)
+}
 
 export const deleteMovie = (id) => {
-    return api.delete(`${baseUrl}/${ROUTES.movies}/${id}`);
-};
+    return api.delete(`${baseUrl}/${ROUTES.movies}/${id}`)
+}
--- src/components/AddMovieModal/AddMovieModal.jsx
+++ src/components/AddMovieModal/AddMovieModal.jsx
@@ -1,57 +1,112 @@
-import React from 'react';
-import PropTypes from "prop-types"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { ErrorMessage, Formik, Field } from 'formik'
 
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { createMovieData } from '../../redux/actions/movieActions';
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { createMovieData } from '../../redux/actions/movieActions'
 
-import Modal from '../Common/Modal/Modal';
+import Modal from '../Common/Modal/Modal'
+import MovieSchema from '../../validationSchemas/movieSchema'
 
-import './AddMovieModal.scss';
+import './AddMovieModal.scss'
 
-const initialState = {
-    title: '',
-    releaseDate: '',
-    movieUrl: '',
-    overview: '',
-    genres: [],
-    runtime: ''
-};
-
-const reducer = (state, { field, value }) => {
-    return {
-        ...state,
-        [field]: value
-    }
+const AddMovieForm = ({ handleSubmit, resetForm }) => {
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label htmlFor="movie-title">TITLE</label>
+                <Field
+                    id="movie-title"
+                    name="title"
+                    placeholder="Movie Title"
+                    type="text"
+                />
+                <ErrorMessage name="title">
+                    {(msg) => <div className="field-error">{msg}</div>}
+                </ErrorMessage>
+            </div>
+            <div className="form-group">
+                <label htmlFor="release-date">RELEASE DATE</label>
+                <div className="release-date-wrapper">
+                    <Field
+                        id="release-date"
+                        name="releaseDate"
+                        placeholder="Select Date"
+                        type="text"
+                    />
+                    <ErrorMessage name="releaseDate">
+                        {(msg) => <div className="field-error">{msg}</div>}
+                    </ErrorMessage>
+                </div>
+            </div>
+            <div className="form-group">
+                <label htmlFor="movie-url">MOVIE URL</label>
+                <Field
+                    id="movie-url"
+                    name="movieUrl"
+                    placeholder="Movie URL here"
+                    type="text"
+                />
+                <ErrorMessage name="movieUrl">
+                    {(msg) => <div className="field-error">{msg}</div>}
+                </ErrorMessage>
+            </div>
+            <div className="form-group">
+                <label htmlFor="movie-genre">GENRE</label>
+                <Field as="select" name="genres" id="movie-genre">
+                    <option value="Drama">Drama</option>
+                    <option value="Comedy">Comedy</option>
+                    <option value="Horror">Horror</option>
+                    <option value="Action">Action</option>
+                    <option value="Crime">Crime</option>
+                    <option value="Thriller">Thriller</option>
+                </Field>
+                <ErrorMessage name="genres">
+                    {(msg) => <div className="field-error">{msg}</div>}
+                </ErrorMessage>
+            </div>
+            <div className="form-group">
+                <label htmlFor="movie-overview">OVERVIEW</label>
+                <Field
+                    id="movie-overview"
+                    name="overview"
+                    placeholder="Overview here"
+                    type="text"
+                />
+                <ErrorMessage name="overview">
+                    {(msg) => <div className="field-error">{msg}</div>}
+                </ErrorMessage>
+            </div>
+            <div className="form-group">
+                <label htmlFor="movie-runtime">RUNTIME</label>
+                <Field
+                    id="movie-runtime"
+                    name="runtime"
+                    placeholder="Runtime here"
+                    type="text"
+                />
+                <ErrorMessage name="runtime">
+                    {(msg) => <div className="field-error">{msg}</div>}
+                </ErrorMessage>
+            </div>
+            <div className="form-footer">
+                <button
+                    className="reset-button"
+                    type="button"
+                    onClick={resetForm}
+                >
+                    RESET
+                </button>
+                <button className="submit-button" type="submit">
+                    SUBMIT
+                </button>
+            </div>
+        </form>
+    )
 }
 
-const AddModal = props => {
-    const [state, dispatch] = React.useReducer(reducer, initialState);
-    const { title, releaseDate, movieUrl, overview, genres, runtime } = state;
-
-    const onChange = (e) => {
-        let value = e.target.name === 'genres' ? [e.target.value] : e.target.value;
-
-        dispatch({ field: e.target.name, value })
-    }
-
-    const onAddMovie = () => {
-        const payload = {
-            title,
-            release_date: releaseDate,
-            poster_path: movieUrl,
-            overview,
-            genres,
-            runtime: Number(runtime)
-        };
-
-        props.createMovieData(payload)
-            .then(() => {
-                props.toggleModalOpen()
-            })
-            .catch(err => console.log(err))
-    }
-
+const AddModal = (props) => {
     return (
         <Modal {...props}>
             <div className="add-movie-container">
@@ -59,62 +114,59 @@ const AddModal = props => {
                     <h2 className="modal-title">ADD MOVIE</h2>
                 </div>
                 <div className="modal-body">
-                    <div className="form-group">
-                        <label htmlFor="movie-title">TITLE</label>
-                        <input id="movie-title" name="title" value={title} placeholder="Movie Title" type="text" onChange={onChange} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="release-date">RELEASE DATE</label>
-                        <div className="release-date-wrapper">
-                            <input id="release-date" name="releaseDate" value={releaseDate} placeholder="Select Date" type="text" onChange={onChange} />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="movie-url">MOVIE URL</label>
-                        <input id="movie-url" name="movieUrl" value={movieUrl} placeholder="Movie URL here" type="text" onChange={onChange} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="movie-genre">GENRE</label>
-                        <div className="movie-genre-wrapper">
-                            <select id="movie-genre" name="genres" value={genres[0]} onChange={onChange}>
-                                <option value="Drama">Drama</option>
-                                <option value="Comedy">Comedy</option>
-                                <option value="Horror">Horror</option>
-                                <option value="Action">Action</option>
-                                <option value="Crime">Crime</option>
-                                <option value="Thriller">Thriller</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="movie-overview">OVERVIEW</label>
-                        <input id="movie-overview" name="overview" value={overview} placeholder="Overview here" type="text" onChange={onChange} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="movie-runtime">RUNTIME</label>
-                        <input id="movie-runtime" name="runtime" value={runtime} placeholder="Runtime here" type="text" onChange={onChange} />
-                    </div>
-                </div>
-                <div className="modal-footer">
-                    <button className="reset-button">RESET</button>
-                    <button className="submit-button" onClick={onAddMovie}>SUBMIT</button>
+                    <Formik
+                        initialValues={{
+                            title: '',
+                            releaseDate: '',
+                            movieUrl: '',
+                            overview: '',
+                            genres: '',
+                            runtime: '',
+                        }}
+                        validationSchema={MovieSchema}
+                        onSubmit={async (values, actions) => {
+                            const {
+                                title,
+                                releaseDate,
+                                movieUrl,
+                                overview,
+                                genres,
+                                runtime,
+                            } = values
+
+                            const payload = {
+                                title,
+                                release_date: releaseDate,
+                                poster_path: movieUrl,
+                                overview,
+                                genres: [genres],
+                                runtime: Number(runtime),
+                            }
+
+                            props
+                                .createMovieData(payload)
+                                .then(() => {
+                                    actions.resetForm()
+                                    props.toggleModalOpen()
+                                })
+                                .catch((err) => console.log(err))
+                        }}
+                    >
+                        {AddMovieForm}
+                    </Formik>
                 </div>
             </div>
         </Modal>
-    );
+    )
 }
 
 AddModal.propTypes = {
     toggleModalOpen: PropTypes.func.isRequired,
-    isModalOpen: PropTypes.bool.isRequired
+    isModalOpen: PropTypes.bool.isRequired,
 }
 
-
 const mapDispatchToProps = (dispatch) => {
-    return bindActionCreators({ createMovieData }, dispatch);
-};
+    return bindActionCreators({ createMovieData }, dispatch)
+}
 
-export default connect(
-    null,
-    mapDispatchToProps
-)(AddModal);
+export default connect(null, mapDispatchToProps)(AddModal)
